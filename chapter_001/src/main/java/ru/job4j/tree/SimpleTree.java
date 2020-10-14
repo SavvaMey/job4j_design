8c1590f0e34966e90f5b076f1c450f627ece202e
SavvaMey <savva28031997@gmail.com>
2020-10-14 19:24:09 +0300

    2. Добавить метод boolean isBinary() [#372808]

--- chapter_001/src/main/java/ru/job4j/tree/SimpleTree.java
+++ chapter_001/src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,13 +1,14 @@
-package tree;
+package ru.job4j.tree;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public interface SimpleTree<E> {
     boolean add(E parent, E child);
 
-    Optional<Node<E>> findBy(E value);
+    Optional<Node<E>> checkTree(Predicate<Node<E>> predicate);
 
     class Node<E> {
         final E value;
@@ -15,6 +16,14 @@ public interface SimpleTree<E> {
 
         public Node(E value) {
             this.value = value;
+        }
+
+        public E getValue() {
+            return value;
+        }
+
+        public List<Node<E>> getChildren() {
+            return children;
         }
     }
 }
--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -3,6 +3,7 @@ package ru.job4j.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
@@ -26,26 +27,22 @@ public class Tree<E> implements SimpleTree<E> {
     }
 
     public boolean isBinary() {
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.children.size() > 2) {
-                return false;
-            }
-            data.addAll(el.children);
-        }
-        return true;
+        Optional<Node<E>> rsl = checkTree(el -> el.children.size() > 2);
+        return rsl.isEmpty();
+    }
+
+    public Optional<Node<E>> findBy(E value) {
+        return checkTree(el -> el.getValue().equals(value));
     }
 
     @Override
-    public Optional<Node<E>> findBy(E value) {
+    public Optional<Node<E>> checkTree(Predicate<Node<E>> predicate) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (predicate.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
