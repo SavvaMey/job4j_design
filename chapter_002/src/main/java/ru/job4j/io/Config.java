2f744751e3080ff2bbfbc30d33eeabc95c86cb95
SavvaMey <savva28031997@gmail.com>
2020-10-22 19:52:21 +0300

    1. Читаем файл конфигурации [#372844]

--- chapter_002/src/main/java/ru/job4j/io/Config.java
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class Config {
     private final String path;
@@ -15,10 +16,18 @@ public class Config {
     }
 
     public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.path))) {
+            in.lines()
+                    .filter(line -> !line.isEmpty() && !line.contains("## ") && line.contains("="))
+                    .map(line -> line.split("="))
+                    .forEach(line -> values.put(line[0],line[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String value(String key) {
-        throw new UnsupportedOperationException("Don't impl this method yet!");
+        return values.get(key);
     }
 
     @Override
@@ -33,6 +42,9 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+        Config file = new Config("app.properties");
+        file.load();
+        System.out.println(file.value("hibernate.connection.username"));
+
     }
 }
