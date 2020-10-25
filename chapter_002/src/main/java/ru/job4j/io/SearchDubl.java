a1e67220c3c4ec95451f42b7b7c1ade26cac1a95
SavvaMey <savva28031997@gmail.com>
2020-10-25 14:53:58 +0300

    4.2. Поиск дубликатов [#372843]

---
+++ chapter_002/src/main/java/ru/job4j/io/SearchDubl.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+
+public class SearchDubl {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start).forEach(System.out::println);
+    }
+
+    public static Set<Path> search(Path root) throws IOException {
+        SearchDublicatesFiles searcher = new SearchDublicatesFiles();
+        Files.walkFileTree(root, searcher);
+        return searcher.getResultPath();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/io/SearchDublicatesFiles.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchDublicatesFiles implements FileVisitor<Path> {
+    Map<Path, Long> storage;
+    Set<Path> resultPath;
+
+    public SearchDublicatesFiles() {
+        storage = new HashMap<>();
+        resultPath = new HashSet<>();
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (storage.containsKey(file.getFileName()) && storage.get(file.getFileName()) == file.toFile().getTotalSpace()) {
+            resultPath.add(file);
+        } else {
+            storage.put(file.getFileName(), file.toFile().getTotalSpace());
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    public Set<Path> getResultPath() {
+        return resultPath;
+    }
+
+}
