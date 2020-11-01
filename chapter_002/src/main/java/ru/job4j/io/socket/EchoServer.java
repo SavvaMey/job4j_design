2ec9283279e3764eb466de804fca9913211d5d6d
SavvaMey <savva28031997@gmail.com>
2020-11-01 14:45:30 +0300

    1. Бот [#372861]

--- chapter_002/src/main/java/ru/job4j/io/socket/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -12,21 +12,27 @@ public class EchoServer {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (true) {
                 boolean status = true;
+                String strStatus = "";
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains("?msg=Bye")) {
-                            status = false;
+                        if (str.contains("?msg=")) {
+                            strStatus= str.split("=")[1].split(" ")[0];
+                            if (strStatus.equals("Exit")) {
+                                status = false;
+                            }
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-                }
-                if (!status) {
-                    break;
+                    if (!status) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        break;
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(strStatus.getBytes());
                 }
             }
         }
