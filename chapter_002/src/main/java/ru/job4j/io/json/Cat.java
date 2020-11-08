53be3bae2e9b8e267123020c52bdb6e7b5b3b13d
SavvaMey <savva28031997@gmail.com>
2020-11-08 13:27:42 +0300

    5. Преобразование JSON в POJO. JsonObject [#372854]

--- chapter_002/src/main/java/ru/job4j/io/json/Cat.java
+++ chapter_002/src/main/java/ru/job4j/io/json/Cat.java
@@ -2,6 +2,8 @@ package ru.job4j.io.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -11,6 +13,26 @@ public class Cat {
     private String name;
     private Contact ownerNumber;
     private String[] features;
+
+    public String getName() {
+        return name;
+    }
+
+    public Contact getOwnerNumber() {
+        return ownerNumber;
+    }
+
+    public String[] getFeatures() {
+        return features;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
 
     public Cat(boolean sex, int weight, String name, Contact ownerNumber, String... features) {
         this.sex = sex;
@@ -35,24 +57,34 @@ public class Cat {
         final Cat cat = new Cat(false, 30, "solo", new Contact("11-111"),
                 "white", "fluffy");
 
-        /* Преобразуем объект cat в json-строку. */
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(cat));
+//        /* Преобразуем объект cat в json-строку. */
+//        final Gson gson = new GsonBuilder().create();
+//        System.out.println(gson.toJson(cat));
+//
+//        /* Модифицируем json-строку */
+//        final String catJson =
+//                "{"
+//                        + "\"sex\":false,"
+//                        + "\"weight\":10,"
+//                        + "\"name\":solo,"
+//                        + "\"ownerNumber\":"
+//                        + "{"
+//                        + "\"phone\":\"+7(924)111-111-11-11\""
+//                        + "},"
+//                        + "\"features\":"
+//                        + "[\"black\",\"bald\"]"
+//                        + "}";
+//        final Cat catMod = gson.fromJson(catJson, Cat.class);
+//        System.out.println(catMod);
 
-        /* Модифицируем json-строку */
-        final String catJson =
-                "{"
-                        + "\"sex\":false,"
-                        + "\"weight\":10,"
-                        + "\"name\":solo,"
-                        + "\"ownerNumber\":"
-                        + "{"
-                        + "\"phone\":\"+7(924)111-111-11-11\""
-                        + "},"
-                        + "\"features\":"
-                        + "[\"black\",\"bald\"]"
-                        + "}";
-        final Cat catMod = gson.fromJson(catJson, Cat.class);
-        System.out.println(catMod);
+        JSONObject jsonOwner = new JSONObject("{\"phone\":" + cat.getOwnerNumber().getPhone() + "}");
+        JSONObject json = new JSONObject();
+        json.put("sex", cat.isSex());
+        json.put("weight", cat.getWeight());
+        json.put("name", cat.getName());
+        json.put("ownerNumber", jsonOwner);
+        json.put("features", new JSONArray(cat.getFeatures()));
+        System.out.println(json.toString());
+        System.out.println(new JSONObject(cat).toString());
     }
 }
