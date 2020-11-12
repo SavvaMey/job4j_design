2a4ad0ee72087d448c58c905972711af6be8bc91
SavvaMey <savva28031997@gmail.com>
2020-11-12 12:53:08 +0300

    6. Кодировка. [#372847]

--- chapter_002/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_002/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -10,6 +10,7 @@ public class ConsoleChat {
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
+    private List<String> answers = new ArrayList<>();
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
@@ -17,51 +18,48 @@ public class ConsoleChat {
     }
 
     public String getBotAnswer() {
-        String answer = "";
-        try (BufferedReader in = new BufferedReader(new FileReader(botAnswers))) {
-            List<String> answers = new ArrayList<>();
-            in.lines().forEach(answers::add);
-            answer = answers.get((int) (Math.random() * answers.size()));
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (answers.isEmpty()) {
+            try (BufferedReader in = new BufferedReader(new FileReader(botAnswers))) {
+                in.lines().forEach(answers::add);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return answer;
+        return answers.get((int) (Math.random() * answers.size()));
     }
 
-    public void run() {
+    public void run() throws FileNotFoundException {
         boolean running = true;
         boolean stop = false;
-            try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-                 PrintWriter out = new PrintWriter(
-                         new BufferedOutputStream(
-                                 new FileOutputStream(path)
-                         ))) {
+        StringBuilder log = new StringBuilder();
+            try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in)))
+            {
                 while (running) {
                 String input = consoleReader.readLine();
                 String bot = getBotAnswer();
                 if (input.equals(OUT)){
                     running = false;
-                    out.write(input + System.lineSeparator());
+                    log.append(input + System.lineSeparator());
                 } else if (input.equals(STOP)) {
                     stop = true;
-                    out.write(input + System.lineSeparator());
+                    log.append(input + System.lineSeparator());
                 } else if (stop && input.equals(CONTINUE)) {
                     stop = false;
                 }
                 if (!stop && running) {
                     System.out.println(bot);
-                    out.write(input + System.lineSeparator()
-                            + bot + System.lineSeparator());
+                    log.append(input + System.lineSeparator() + bot + System.lineSeparator());
                 }
             }
-        } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+        } catch (IOException e) {
                 e.printStackTrace();
             }
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(path)))) {
+            out.write(String.valueOf(log));
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ConsoleChat cc = new ConsoleChat("./chapter_002/src/data/log.txt",
                 "./chapter_002/src/data/text.txt");
         cc.run();
