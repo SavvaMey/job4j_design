da08a2bf4c5b86ded30a6a8bedc928cc6a65d222
SavvaMey <savva28031997@gmail.com>
2020-12-08 15:54:01 +0300

    0. Видео [#372749]

--- chapter_003/src/main/java/ConnectionDemo.java
+++ chapter_003/src/main/java/ConnectionDemo.java
@@ -1,34 +1,45 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        List<String> lines= load();
+        Settings settings = new Settings();
+        ClassLoader loader = Settings.class.getClassLoader();
+        try (InputStream io = loader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Class.forName("org.postgresql.Driver");
-        String url = lines.get(0);
-        String login = lines.get(1);
-        String password = lines.get(2);
+        String url = settings.getValue("jdbc.url");
+        String login = settings.getValue("jdbc.login");
+        String password = settings.getValue("jdbc.password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
         }
     }
-    public static List<String> load() {
-        String path = "./chapter_003/data/app.properties";
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-            lines = in.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+
+    static class Settings {
+        private final Properties prs = new Properties();
+
+        public void load (InputStream io) {
+            try {
+                this.prs.load(io);
+            } catch ( Exception e) {
+                e.printStackTrace();
+            }
         }
-        return lines;
+        public String getValue(String key) {
+            return this.prs.getProperty(key);
+        }
     }
 }
