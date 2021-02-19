b8292fb9b9ed43d7c9c30b0f97a04ada2deb3a58
SavvaMey <savva28031997@gmail.com>
2021-02-20 00:53:10 +0300

    SQL [#224183]

--- chapter_003/data/meetings.sql
+++ chapter_003/data/meetings.sql
@@ -14,6 +14,7 @@ create table user_meeting (
     status varchar(255),
     CONSTRAINT order_details_pk PRIMARY KEY (user_id, meeting_id)
 );
+
 insert into users(name) values ('sav');
 insert into users(name) values ('savva');
 insert into users(name) values ('koja');
@@ -21,12 +22,13 @@ insert into users(name) values ('sas');
 insert into users(name) values ('ivan');
 insert into users(name) values ('zxcv');
 insert into users(name) values ('qwer');
-insert into users(name) values ('opa');
+insert into users(name) values ('lev');
 
 insert into meetings(name) values ('arch_meet');
 insert into meetings(name) values ('debian_meet');
 insert into meetings(name) values ('ubuntu_meet');
 insert into meetings(name) values ('win_meet');
+insert into meetings(name) values ('MsDOS_meet');
 
 insert into user_meeting(user_id, meeting_id, status) values (1, 2, 'ok');
 insert into user_meeting(user_id, meeting_id, status) values (1, 3, 'no');
@@ -39,3 +41,11 @@ insert into user_meeting(user_id, meeting_id, status) values (5, 2, 'no');
 insert into user_meeting(user_id, meeting_id, status) values (6, 1, 'ok');
 insert into user_meeting(user_id, meeting_id, status) values (7, 3, 'no');
 insert into user_meeting(user_id, meeting_id, status) values (8, 4, 'ok');
+select * from user_meeting;
+
+/*Нужно написать запрос, который получит список всех заяков и количество подтвердивших участников.*/
+select m.name, count(uM.status) from meetings m left join user_meeting
+    uM on m.id = uM.meeting_id AND uM.status = 'ok' group by m.name;
+/*Нужно получить все совещания, где не было ни одной заявки на посещения */
+select m.name from meetings m left join user_meeting
+    uM on m.id = uM.meeting_id  group by m.name having count(uM.status) = 0;
